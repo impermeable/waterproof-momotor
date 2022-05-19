562ad83387b7e779bedd2df91d64480b66b137c0
SeanMcCarren <mccarrensean@gmail.com>
2022-05-19 11:21:56 +0200

    Create test_coqc.py

---
+++ test/test_coqc.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from waterproof_momotor._formatting import wp_formatter, lemmas
+from waterproof_momotor.io._import import read
+from waterproof_momotor._coqc import coqc
+
+def test_coqc_tutorial():
+    test_file = Path(__file__).parent / 'io' / 'test_porting' / 'tutorial.wpe'
+    notebook = read(test_file)
+    v = wp_formatter(notebook)
+    result = coqc(v)
+    assert result.returncode == 1
+
+    print(result.stdout.decode('utf8', errors='ignore').encode('utf8'))
+    print(result.stderr)
+
+def test_lemmas_tutorial():
+    test_file = Path(__file__).parent / 'io' / 'test_porting' / 'tutorial.wpe'
+    notebook = read(test_file)
+    lmms = lemmas(notebook)
