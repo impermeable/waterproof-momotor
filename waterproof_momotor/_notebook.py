72c670b78fc14d791f986fab0c05efa3556cd96f
SeanMcCarren <mccarrensean@gmail.com>
2022-05-19 11:21:24 +0200

    Add blocks to notebooks

--- waterproof_momotor/_notebook.py
+++ waterproof_momotor/_notebook.py
@@ -1,4 +1,27 @@
+from typing import List
+
+class Block:
+    block_type: str
+    text: str
+    start: bool
+    id: str
+
+    def __init__(self, block_type, text=None, start=None, id=None):
+        self.block_type = block_type
+        self.text = text
+        self.start = start
+        self.id = id
 
 class Notebook:
-    def __init__(self):
-        return
+    blocks: List[Block]
+
+    def __init__(self, blocks):
+        self.blocks = blocks
+        self._validate()
+
+    def _validate(self):
+        assert isinstance(self.blocks, list)
+
+    def code_blocks(self):
+        return [block.text for block in self.blocks if block.block_type == "code"]
+
