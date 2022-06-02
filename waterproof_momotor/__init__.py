6972171d534250def530b6a3dd8fa7596e6e2228
SeanMcCarren <mccarrensean@gmail.com>
2022-06-02 12:13:55 +0200

    Update __init__.py

--- waterproof_momotor/__init__.py
+++ waterproof_momotor/__init__.py
@@ -1,5 +1,13 @@
 from ._notebook import Notebook
+from ._formatting import wp_formatter, lemma_names, lemmas
+from ._coqc import coqc
+from .io._import import load_file
 
 __all__ = [
     "Notebook",
+    "wp_formatter",
+    "lemma_names",
+    "lemmas",
+    "coqc",
+    "load_file"
 ]
