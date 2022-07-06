f863922256334e0d39a689d7f1f50fe42a4a3278
SeanMcCarren <mccarrensean@gmail.com>
2022-07-06 11:29:40 +0200

    Update __init__.py

--- waterproof_momotor/__init__.py
+++ waterproof_momotor/__init__.py
@@ -3,14 +3,22 @@ from .util._formatting import wp_formatter, lemma_names, lemmas, insert_lemma
 from .util._coqc import coqc
 from .util._import import load_file
 from .checklets.recipe import recipe
+from .checklets.check_notebook_integrity import CheckletNotebookIntegrity
+from .checklets.full_run_coqc import CheckletFullCoqc
+from .checklets.single_cell_coqc import CheckletSingleCellCoqc
+from .checklets.single_cell_manual_checks import CheckletSingleCellText
 
 __all__ = [
     "Notebook",
     "wp_formatter",
     "lemma_names",
     "lemmas",
-    "insert_lemma"
+    "insert_lemma",
     "coqc",
     "load_file",
-    "recipe"
+    "recipe",
+    "CheckletNotebookIntegrity",
+    "CheckletFullCoqc",
+    "CheckletSingleCellCoqc",
+    "CheckletSingleCellText"
 ]
