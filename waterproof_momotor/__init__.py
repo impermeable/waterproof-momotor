6f300872f629619ea9f2d9b05e6bf5604cc0202d
SeanMcCarren <mccarrensean@gmail.com>
2022-07-06 11:31:11 +0200

    Update __init__.py

--- waterproof_momotor/__init__.py
+++ waterproof_momotor/__init__.py
@@ -3,6 +3,7 @@ from .util._formatting import wp_formatter, lemma_names, lemmas, insert_lemma
 from .util._coqc import coqc
 from .util._import import load_file
 from .checklets.recipe import recipe
+from .checklets.checklet import CheckletReport
 from .checklets.check_notebook_integrity import CheckletNotebookIntegrity
 from .checklets.full_run_coqc import CheckletFullCoqc
 from .checklets.single_cell_coqc import CheckletSingleCellCoqc
@@ -17,6 +18,7 @@ __all__ = [
     "coqc",
     "load_file",
     "recipe",
+    "CheckletReport",
     "CheckletNotebookIntegrity",
     "CheckletFullCoqc",
     "CheckletSingleCellCoqc",
