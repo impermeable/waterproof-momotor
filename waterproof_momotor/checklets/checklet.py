69d2bef5aad5189fdd7c06e003bed5b5a6e7afa0
SeanMcCarren <mccarrensean@gmail.com>
2022-08-09 13:56:17 +0200

    remove old

--- waterproof_momotor/checklets/checklet.py
+++
@@ -1,7 +0,0 @@
-class CheckletReport:
-    def __init__(self, result):
-        assert isinstance(result, bool)
-        self.result = result
-        # json stuff?
-        # HTML formatted output?
-        # I'm not sure what should be here.
