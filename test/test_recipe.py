0da02f5ba1e82be0ac40a1fdd63827cc387bc790
SeanMcCarren <mccarrensean@gmail.com>
2022-08-09 13:57:11 +0200

    Delete test_recipe.py

--- test/test_recipe.py
+++
@@ -1,6 +0,0 @@
-from waterproof_momotor import recipe
-from pathlib import Path
-test_file = Path(__file__).parent / 'io' / 'test_porting' / 'tutorial.wpe'
-
-def test_recipe():
-    assert recipe(solution_file=test_file, student_files=[test_file])
