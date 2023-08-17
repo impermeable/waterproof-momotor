ad38adfcb1acb6dd4e49f33791d09f070ed0957e
jim_jwp <j.w.portegies@tue.nl>
2023-08-17 16:07:33 +0200

    Allow for importing .mv files

--- waterproof_momotor/_import.py
+++ waterproof_momotor/_import.py
@@ -1,16 +1,17 @@
 from ._notebook import Notebook, Block
 import json
+import re
 
 
 def load_file(file, extention="auto") -> Notebook:
     """
-    Load a waterproof notebook/exercisesheet or .v file into a Notebook object.
+    Load a waterproof notebook/exercisesheet or .v or .mv file into a Notebook object.
 
     Parameters
     ----------
     file : file object, Path, str
         Reads content of file object or reads concent at string location
-    extention : {"auto", "wpn", "wpe", "v"}
+    extention : {"auto", "wpn", "wpe", "v", "mv"}
         The extention of the file. If not auto, this ensures a certain type is expected.
     """
     if hasattr(file, "read"):
@@ -30,6 +31,8 @@ def load_file(file, extention="auto") -> Notebook:
 
     if extention == "v":
         return _load_file_v(content)
+    if extention == 'mv':
+        return _load_file_mv(content)
     elif extention in ["wpn", "wpe"]:
         return _load_file_wp(content)
     else:
@@ -57,3 +60,37 @@ def _load_file_wp(txt):
 def _load_file_v(txt):
     # TODO implement as it is not (yet?) needed.
     raise NotImplementedError()
+
+def _load_file_mv(txt):
+    input_blocks = re.split('(```coq.*?```|<input-area>|</input-area>)', txt, flags=re.DOTALL)
+    blocks = []
+    id = 1
+    for input_block in input_blocks:
+        if input_block == '<input-area>':
+            block = Block(
+                block_type = "input",
+                start = True,
+                id = id
+            )
+        elif input_block == '</input-area>':
+            block = Block(
+                block_type = "input",
+                start = False,
+                id = id
+            )
+            id+=1
+        else:
+            my_match = re.match('```coq(.*?)```', input_block, flags=re.DOTALL)
+            if my_match:
+                block = Block(
+                    block_type = "code",
+                    text = my_match.group(1)
+                )
+            else:
+                block = Block(
+                    block_type = "text",
+                    text = input_block
+                )
+        blocks.append(block)
+    nb = Notebook(blocks)
+    return nb
